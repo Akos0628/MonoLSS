4ceecc436e4fa63f5bd8c3df0163c47d68f144ee
Akos0628 <75949273+Akos0628@users.noreply.github.com>
2024-09-24 17:13:12 +0200

    first test eval

--- Install.md
+++ Install.md
@@ -1,6 +1,6 @@
 
 ```bash
-conda create -n monolss python=3.7 pytorch==1.3.1 torchvision==0.4.2 cudatoolkit=11.1 -c pytorch -c conda-forge
+conda create -n monolss python=3.9 pytorch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 pytorch-cuda=12.4 -c pytorch -c nvidia
 ```
 
 ```bash
@@ -9,4 +9,16 @@ conda activate monolss
 
 ```bash
 pip3 install -r requirement.txt
+```
+
+
+
+usefull if something doesn't work:
+
+```bash
+conda info --envs
+```
+
+```bash
+conda remove -n monssl --all
 ```
--- tools/train_val.py
+++ tools/train_val.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import torch
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
@@ -34,7 +35,22 @@ def create_logger(log_file):
     return logging.getLogger(__name__)
 
 
-def main():
+def main():   
+    #assert (torch.cuda.is_available())
+
+    # setting device on GPU if available, else CPU
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device:', device)
+    print()
+
+    #Additional Info when using cuda
+    if device.type == 'cuda':
+        print(torch.cuda.get_device_name(0))
+        print('Memory Usage:')
+        print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
+
+
     # load cfg
     assert (os.path.exists(args.config))
     cfg = yaml.load(open(args.config, 'r'), Loader=yaml.Loader)
